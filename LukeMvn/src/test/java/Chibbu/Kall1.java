7c7875db8ce472bb41e5a6b6314f775b75c26d62
Harish246 <srihari224466@gmail.com>
2022-10-19 16:51:28 +0530

    b

--- LukeMvn/src/test/java/Chibbu/Kall1.java
+++ LukeMvn/src/test/java/Chibbu/Kall1.java
@@ -4,6 +4,7 @@ public class Kall1 {
 
 	public static void main(String[] args) {
 		System.out.println("Chibbu kala son");
+		System.out.println("Chibbu kala son");
 
 	}
 
--- LukeMvn/src/test/java/Luk.java
+++
@@ -1,8 +0,0 @@
-
-public class Luk {
-
-	public static void main(String[] args) {
-		System.out.println("My uncle Luk");
-	}
-
-}
